cba7efce41dfe1af8000b4e3f732b5e23f8bbe14
iamEddielo <iameddielopez@gmail.com>
2020-08-06 14:30:36 -0400

    finished commit

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize (name, owner_obj)
+    @name = name 
+    @owner = owner_obj
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
